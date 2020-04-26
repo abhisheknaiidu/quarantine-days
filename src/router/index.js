5719c8c022a4e763d01c30a03814608e4ba17988
abhisheknaiidu <geekyabhi12@gmail.com>
2020-04-26 08:31:11 +0530

    Added subreddit route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Subreddits from '../views/Subreddits.vue';
+import Subreddit from './views/Subreddit.vue';
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,11 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+  },
+  {
+    path: '/r/:name',
+    name: 'subreddit',
+    component: Subreddit,
   },
 ];
 
