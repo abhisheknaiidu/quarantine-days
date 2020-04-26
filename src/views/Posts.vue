58f57dabb4691cb6fa94fc6430b1b1e24e6a2057
abhisheknaiidu <geekyabhi12@gmail.com>
2020-04-26 07:17:06 +0530

    Updatedd Posts.vue page

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,3 +1,15 @@
 <template>
 <h1> Abhishek </h1>
 </template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  mounted() {
+    this.init();
+  },
+  computed: mapState('subreddits', ['subreddits']),
+  methods: mapActions('subreddits', ['init']),
+};
+</script>
