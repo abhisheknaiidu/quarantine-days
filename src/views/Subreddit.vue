7a728a623bb096f29831bbb81e40ab2f953594b4
abhisheknaiidu <geekyabhi12@gmail.com>
2020-05-02 22:56:10 +0530

    Cards working

--- src/views/Subreddit.vue
+++ src/views/Subreddit.vue
@@ -40,12 +40,6 @@
                   </div>
                   <div class="card-content">
                       <div class="media">
-                          <div class="media-left">
-                              <figure class="image is-48x48">
-                                  <img :src="loadedUsersById[post.user_id].image"
-                                      alt="Placeholder image">
-                              </figure>
-                          </div>
                           <div class="media-content">
                               <p class="title is-4"
                                   v-if="!post.URL">{{post.title}}</p>
