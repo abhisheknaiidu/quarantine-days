459c3bbca5d90cfb8688394422f848a37c03f38b
abhisheknaiidu <geekyabhi12@gmail.com>
2020-05-02 22:47:17 +0530

    Added toggle and cards

--- src/views/Subreddit.vue
+++ src/views/Subreddit.vue
@@ -1,7 +1,13 @@
  <template>
      <section>
-     <h1> {{ subreddit.name }} </h1>
-      <form @submit.prevent="onCreatePost()">
+     <button
+        @click="showForm = !showForm"
+        class="button is-primary">
+        Toggle Form
+      </button>
+      <form
+        v-if="showForm"
+        @submit.prevent="onCreatePost()">
        <b-field label="Title">
           <b-input
           v-model="post.title"
@@ -20,6 +26,53 @@
       </b-field>
     <button class="button is-success">Add Post</button>
     </form>
+    <div class="posts columns is-multiline is-4">
+          <div class="column is-4"
+              v-for="post in posts"
+              :key="post.id">
+              <div class="card">
+                  <div class="card-image"
+                      v-if="isImage(post.URL)">
+                      <figure class="image">
+                          <img :src="post.URL"
+                              alt="Placeholder image">
+                      </figure>
+                  </div>
+                  <div class="card-content">
+                      <div class="media">
+                          <div class="media-left">
+                              <figure class="image is-48x48">
+                                  <img :src="loadedUsersById[post.user_id].image"
+                                      alt="Placeholder image">
+                              </figure>
+                          </div>
+                          <div class="media-content">
+                              <p class="title is-4"
+                                  v-if="!post.URL">{{post.title}}</p>
+                              <p class="title is-4"
+                                  v-if="post.URL">
+                                  <a :href="post.URL"
+                                      target="_blank">{{post.title}}</a>
+                              </p>
+                          </div>
+                      </div>
+                      <div class="content">
+                          {{post.description}}
+                          <br>
+                          <!-- <router-link
+                            :to="{
+                              name: 'post',
+                              params: {
+                                name: $route.params.name,
+                                post_id: post.id
+                              }
+                            }"
+                            class="button is-primary">View Post</router-link> -->
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   </section>
 </template>
 
@@ -28,6 +81,7 @@ import { mapState, mapGetters, mapActions } from 'vuex';
 
 export default {
   data: () => ({
+    showForm: false,
     post: {
       title: '',
       description: '',
@@ -54,6 +108,9 @@ export default {
     ...mapGetters('subreddit', ['subreddit']),
   },
   methods: {
+    isImage(url) {
+      return url.match(/(png|jpg|jpeg|gif)$/);
+    },
     ...mapActions('subreddit', ['createPost', 'initSubreddit', 'initPosts']),
     async onCreatePost() {
       if (this.post.title && (this.post.description || this.post.URL)) {
@@ -63,3 +120,19 @@ export default {
   },
 };
 </script>
+
+<style>
+
+.posts {
+   margin-top: 2em;
+    }
+.card {
+   height: 100%;
+   margin: 1%;
+   border-radius: 5px;
+    }
+.card img {
+   border-radius: 5px;
+    }
+
+</style>
