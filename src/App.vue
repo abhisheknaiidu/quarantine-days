b0ed1c8a9b21f0fe035887a8e21bc11690d5668b
abhisheknaiidu <geekyabhi12@gmail.com>
2020-05-02 23:41:39 +0530

    Added title

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <nav class="navbar is-black" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="/#/subreddits">
-          Seedling 🌱
+          Quarantine-Days 🌱
         </a>
 
       </div>
